fe5a9e7c398975a57997db10768c25130674453d
Chris Filiatrault <chris Filiatrault@hotmail.com>
2020-09-03 21:43:13 +1000

    Initial Commit

---
+++ SwiftUI-Alert-With-Textfield/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Alert-With-Textfield
+//
+//  Created by Chris Filiatrault on 3/9/20.
+//  Copyright © 2020 Chris Filiatrault. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
